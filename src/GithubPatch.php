3c84ca16a4615fa0245588d02925ed0a69ff3094
Richard Jones <rich@linuxplicable.org>
2014-01-28 22:18:29 +0000

    Do some applyin'

--- src/GithubPatch.php
+++ src/GithubPatch.php
@@ -16,13 +16,60 @@ class GithubPatch extends Shared {
 
     public function process($message) {
         $response = new \stdClass;
-        $response->type = 'github_patch';
+        $response->type = 'complete';
         $response->status = 'error';
         $response->payload = 'processing error';
         self::$log->addDebug('processing message : ' . print_r($message, true));
 
-        $response->status = 'success';
-        $response->payload = $patch;
+        try {
+            $requiredProperties = array(
+                'patch' => true,
+                'before' => true,
+                'after' => true,
+                'user' => true,
+                'repo' => true,
+            );
+            $this->validateArray($requiredProperties, $message);
+        } catch (\Exception $e) {
+            $response->payload = $e->getMessage();
+            return json_encode($response);
+        }
+        self::$log->addDebug('data : ' . print_r($this->data, true));
+
+        //Find where the working copy is
+        if (!array_key_exists('repo_' . $this->data['user'] . '_' . $this->data['repo'])) {
+            $reponse->payload = 'No location for ' . $this->data['user'] . ':' . $this->data['repo'];
+            return json_encode($response);
+        }
+        $target_dir = self::$config['repo_' . $this->data['user'] . '_' . $this->data['repo']];
+        if (!is_dir($target_dir)) {
+            $response->payload = $target_dir . ' does not exist';
+            return json_encode($response);
+        }
+
+        //Check the current revision
+        $command = '/usr/bin/git --git-dir=' . $target_dir . '/.git --work-tree=' . $target_dir . ' log -n 1 --pretty=format:%H';
+        self::$log->addDebug($command);
+        $output = exec($command);
+        self::$log->addDebug($output);
+
+        //Check that before == current
+        if ($output === $this->data['before']) {
+            $response->payload = 'Not at correct patch level : wc @ ' . $output . ', patch starts @ ' . $this->data['before'];
+            return json_encode($response);
+        }
+
+        //Write patch to temp file
+        $filename = tempnam(self::$config['repo_' . $this->data['user'] . '_' . $this->data['repo']], 'patch_');
+        file_put_contents($filename, $this->data['patch']);
+
+        //Try to process as a github patch
+        $command = '/usr/bin/git --git-dir=' . $target_dir . '/.git --work-tree=' . $target_dir . ' am < ' . $filename;
+        self::$log->addDebug($command);
+        $output = exec($command);
+        self::$log->addDebug($output);
+        unlink($filename);
+        
         return json_encode($response);
 
     }
