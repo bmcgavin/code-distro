c7a239f9e34892d690aa877ab51db039412543d8
Richard Jones <rich@linuxplicable.org>
2014-02-24 22:10:01 +0000

    Allow a real payload

--- test/generate_post.php
+++ test/generate_post.php
@@ -6,14 +6,18 @@ $queue = new ZMQSocket($context, ZMQ::SOCKET_REQ);
 
 $queue->connect("tcp://127.0.0.1:5555");
 
-$payload = new stdClass;
-$payload->ref = 'ref/heads/master';
-$payload->before = '123';
-$payload->after  = '456';
-$payload->repository = array(
-    'url' => 'http://localhost/nowhere'
-);
-$payload = json_encode($payload);
+if (array_key_exists(1, $argv)) {
+    $payload = $argv[1];
+} else {
+    $payload = new stdClass;
+    $payload->ref = 'ref/heads/master';
+    $payload->before = '123';
+    $payload->after  = '456';
+    $payload->repository = array(
+        'url' => 'http://localhost/nowhere'
+    );
+    $payload = json_encode($payload);
+}
 
 try {
         $queue->send(json_encode(array('type' => 'github_hook', 'payload' => $payload)));
