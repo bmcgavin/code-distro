ebba1fbe1903c2b80aecc6f4ab483e2edbea0aed
Richard Jones <rich@linuxplicable.org>
2014-02-27 21:43:10 +0000

    Backtick launches in different shell (gitorious?) on pinata, so change -ax to ax

--- bin/stop_all.sh
+++ bin/stop_all.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-pslist=`ps -ax -o 'pid,command' | grep -v grep | grep php | grep client.php | awk '{ print $1 }'`
+pslist=`ps ax -o 'pid,command' | grep -v grep | grep php | grep client.php | awk '{ print $1 }'`
 
 for pid in $pslist
 do
     kill $pid
 done
-pslist=`ps -ax -o 'pid,command' | grep -v grep | grep php | grep server.php | awk '{ print $1 }'`
+pslist=`ps ax -o 'pid,command' | grep -v grep | grep php | grep server.php | awk '{ print $1 }'`
 
 for pid in $pslist
 do
