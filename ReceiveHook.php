b8bc00008c304f3418741aa381045c3d25889cb5
Richard Jones <rich@linuxplicable.org>
2014-04-22 21:03:43 +0100

    Better hooks and encryption

--- ReceiveHook.php
+++ ReceiveHook.php
@@ -1,13 +1,33 @@
 <?php
 
+$fh = fopen('/var/www/ghcb.linuxplicable.org/logs/log', 'a');
+fwrite($fh, 'Got message : ' . $_POST['payload'] . PHP_EOL);
+
 $context = new ZMQContext();
 $queue = new ZMQSocket($context, ZMQ::SOCKET_REQ);
 
 $queue->connect("tcp://127.0.0.1:5555");
 
-try {
-    $queue->send(json_encode(array('type' => 'github_hook', 'payload' => $_POST['payload'])));
-} catch (Exception $e) {
-    error_log($e->getMessage());
+require '/tmp/patcher/bmcgavin/code-distro/src/Encryption.php';
+
+$type = 'GithubHook';
+
+$_POST['payload'] = json_decode($_POST['payload']);
+
+//Change message type based on format
+if (property_exists($_POST['payload'], 'canon_url')) {
+    $type = 'BitbucketHook';
 }
+
+$e = new \Codedistro\Encryption('/etc/code-distro/key');
+$_POST['payload'] = $e->encrypt($_POST['payload']);
+
+
+try {
+	$queue->send(json_encode(array('type' => $type, 'payload' => $_POST['payload'])));
+} catch (Exception $e) {
+	error_log($e->getMessage());
+}
+fwrite($fh, 'Sent' . PHP_EOL);
+fclose($fh);
 
